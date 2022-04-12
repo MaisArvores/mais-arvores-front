78553a34d3d60b72033c7f67cfeda3bb5da3ae4f
unknown <32620@fiecalunos.lan>
2022-04-11 22:32:57 -0300

    home-tcc

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <p>Entre em contato com a gente</p>
+    </footer>
+);
+
+export default Footer;
---
+++ src/pages/sobre.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Sobre = () => (
+  <div className="title">
+    <h4>Login</h4>
+    <p></p>
+  </div>
+);
+
+export default Sobre;
